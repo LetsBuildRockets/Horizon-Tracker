2f0560de37b381eab42017a8451a1c8ab8181782
Pooja <19swamipo@fpsct.org>
2017-07-12 15:16:02 -0400

    moved everything to horizonTracker and made different functions to make it clearer and started to work with video

---
+++ horizonTracker.cpp
@@ -0,0 +1,119 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <math.h>
+
+#define PI 3.14159
+#define contrastValue 2.7
+#define erodeValue 10
+#define dilateValue 20
+#define epsilonValue 10
+
+cv::Mat erodeKernel = cv::getStructuringElement( cv::MORPH_ELLIPSE,
+                                    cv::Size( 2*erodeValue + 1, 2*erodeValue+1 ),
+                                    cv::Point( erodeValue, erodeValue ) );
+cv::Mat dilateKernel = cv::getStructuringElement( cv::MORPH_ELLIPSE,
+                                        cv::Size( 2*dilateValue + 1, 2*dilateValue+1 ),
+                                        cv::Point( dilateValue, dilateValue ) );
+
+cv::Mat processVideo(cv::Mat);
+std::vector<std::vector<cv::Point> > findBiggestThree(cv::Mat);
+double getAngleFromLargestLine(std::vector<std::vector<cv::Point> >);
+
+int main(int, char**)
+{
+    cv::VideoCapture cap("/home/developer/Documents/Horizon-Tracker/HorizonVideo.avi");
+    if(!cap.isOpened())
+       return -1;
+
+   //cv::namedWindow("Horizon Tracker",1);
+   for(;;)
+   {
+       cv::Mat frame;
+       cap >> frame;
+       /*cv::Mat canny = processVideo(frame);
+       std::vector<std::vector<cv::Point> > biggestThreeContours = findBiggestThree(canny);
+       double angleFromLine = getAngleFromLargestLine(biggestThreeContours);
+       std::cout << angleFromLine << std::endl;*/
+       //imshow("Horizon Tracker", canny);
+       if(cv::waitKey(30) >= 0) break;
+   }
+   return 0;
+}
+
+cv::Mat processVideo(cv::Mat src)
+{
+  cv::Mat dst;
+  src.convertTo(dst, -1, contrastValue, 0);
+  cv::medianBlur(dst, dst, 3);
+  cv::Sobel(dst, dst, -1, 1, 1, 7);
+  cv::inRange(dst, cv::Scalar(0, 0, 255), cv::Scalar(0, 0, 255), dst);
+  cv::dilate(dst, dst, dilateKernel);
+  cv::erode(dst, dst, erodeKernel, cv::Point(-1, -1), 2);
+  cv::Canny(dst, dst, 0, 255, 3);
+  return dst;
+}
+
+std::vector<std::vector<cv::Point> > findBiggestThree(cv::Mat cannyMatrix)
+{
+  std::vector<std::vector<cv::Point> > contours;
+  std::vector<cv::Vec4i> hierarchy;
+  std::vector<cv::Point> emptyContour;
+  std::vector<std::vector<cv::Point> > biggestThree (3, emptyContour);
+  cv::findContours(cannyMatrix, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE, cv::Point(0,0));
+  for (int i = 0; i < contours.size(); i++)
+  {
+    if(contours[i].size() > biggestThree[2].size())
+    {
+      if(contours[i].size() > biggestThree[1].size())
+      {
+        if(contours[i].size() > biggestThree[0].size())
+        {
+          biggestThree.insert(biggestThree.begin()+0, contours[i]);
+          biggestThree.erase(biggestThree.begin()+4);
+        }
+        else {
+        biggestThree.insert(biggestThree.begin()+1, contours[i]);
+        biggestThree.erase(biggestThree.begin()+4);
+        }
+      }
+        else {
+          biggestThree.insert(biggestThree.begin()+2, contours[i]);
+          biggestThree.erase(biggestThree.begin()+4);
+        }
+      }
+
+  }
+  return biggestThree;
+}
+double getAngleFromLargestLine(std::vector<std::vector<cv::Point> > biggestThree)
+{
+  std::vector<std::vector<cv::Point> > approximatedContours;
+  approximatedContours.resize(3);
+  cv::approxPolyDP(biggestThree[0], approximatedContours[0], epsilonValue, false);
+  cv::approxPolyDP(biggestThree[1], approximatedContours[1], epsilonValue, false);
+  cv::approxPolyDP(biggestThree[2], approximatedContours[2], epsilonValue, false);
+  //cv::drawContours(dst, biggestThree, -1, cv::Scalar(100, 100, 100), 2);
+  int largestDistance = 0;
+  cv::Point startPoint;
+  cv::Point endPoint;
+
+  for(int x = 0; x < 3; x++)
+  {
+    for(int i = 0; i < approximatedContours[x].size()-1; i++)
+    {
+      int distance = (approximatedContours[x][i].x - approximatedContours[x][i+1].x) + (approximatedContours[x][i].y - approximatedContours[x][i+1].y);
+      if (distance > largestDistance)
+      {
+        largestDistance = distance;
+        startPoint.x = approximatedContours[x][i].x;
+        startPoint.y = approximatedContours[x][i].y;
+        endPoint.x = approximatedContours[x][i+1].x;
+        endPoint.y = approximatedContours[x][i+1].y;
+      }
+    }
+  }
+  double opposite = endPoint.y-startPoint.y;
+  double adjacent = endPoint.x-startPoint.x;
+  double angle = -atan(opposite/adjacent)*180/PI;
+  return angle;
+}
