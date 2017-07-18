a89befc2eae4a413cea4e4e6b54a17654b9a0e2d
Pooja <19swamipo@fpsct.org>
2017-07-18 14:52:55 -0400

    changed some stuff to use hough

--- horizonTracker.cpp
+++ horizonTracker.cpp
@@ -8,6 +8,11 @@
 #include <string>
 #include <sstream>
 
+int thresholdValue;
+int minLineValue;
+int maxLineValue;
+cv::Mat frame;
+
 using easywsclient::WebSocket;
 static WebSocket::pointer ws = NULL;
 
@@ -27,6 +32,8 @@ cv::Mat dilateKernel = cv::getStructuringElement( cv::MORPH_ELLIPSE,
 cv::Mat processVideo(cv::Mat);
 std::vector<std::vector<cv::Point> > findBiggestThree(cv::Mat);
 double getAngleFromLargestLine(std::vector<std::vector<cv::Point> >);
+void transformUsingHough(cv::Mat);
+void on_trackbar(int, void*);
 
 void handle_message(const std::string & message)
 {
@@ -36,51 +43,79 @@ void handle_message(const std::string & message)
 
 int main(int argc, char** argv)
 {
-    cv::Mat frame  = cv::imread(argv[1]);
+    frame  = cv::imread(argv[1]);
     //if(!cap.isOpened())
     //   return -1;
-    ws = WebSocket::from_url("ws://localhost:8126/foo", std::string());
-    assert(ws);
-   cv::namedWindow("Horizon Tracker",1);
-   for(;;)
-   {
+    //ws = WebSocket::from_url("ws://localhost:8126/foo", std::string());
+    //assert(ws);
+   cv::namedWindow("Lines",1);
+   thresholdValue = 85;
+   minLineValue = 50;
+   maxLineValue = 60;
+   cv::createTrackbar("Threshold Trackbar", "Lines", &thresholdValue, 100, on_trackbar);
+   cv::createTrackbar("MinLineLength Trackbar", "Lines", &minLineValue, 200, on_trackbar);
+   cv::createTrackbar("MaxLineGap Trackbar", "Lines", &maxLineValue, 200, on_trackbar);
+   on_trackbar(0, 0);
+   //for(;;)
+   //{
 
       // cv::Mat frame;
       // cap >> frame;
-       cv::Mat canny = processVideo(frame);
+    /*   cv::Mat canny = processVideo(frame);
        std::vector<std::vector<cv::Point> > biggestThreeContours = findBiggestThree(canny);
-       double angleFromLine = getAngleFromLargestLine(biggestThreeContours);
-       std::cout << angleFromLine << std::endl;
-       std::ostringstream strs;
-       strs << angleFromLine;
-        ws->send(strs.str());
+       double angleFromLine = getAngleFromLargestLine(biggestThreeContours); */
+    //   std::cout << angleFromLine << std::endl;
+      // std::ostringstream strs;
+       //strs << angleFromLine;
+        //ws->send(strs.str());
         //while (ws->getReadyState() != WebSocket::CLOSED) {
-          ws->poll();
-          ws->dispatch(handle_message);
+          //ws->poll();
+          //ws->dispatch(handle_message);
           //std::cout << "i'm stuck" << std::endl;
         //}
-       imshow("Horizon Tracker", canny);
+       /*imshow("Horizon Tracker", canny);
        int keyCode = cv::waitKey(30);
        if(keyCode >= 0 && keyCode != 255) {
          std::cout << keyCode << std::endl;
          break;
        }
    }
-   delete ws;
+   delete ws;*/
+   cv::waitKey(0);
    return 0;
 }
-
+void on_trackbar(int, void*)
+{
+  cv::Mat canny = processVideo(frame);
+  std::vector<std::vector<cv::Point> > biggestThreeContours = findBiggestThree(canny);
+  double angleFromLine = getAngleFromLargestLine(biggestThreeContours);
+}
 cv::Mat processVideo(cv::Mat src)
 {
   cv::Mat dst;
   src.convertTo(dst, -1, contrastValue, 0);
-  cv::medianBlur(dst, dst, 3);
+  cv::medianBlur(dst, dst, 11);
   cv::Sobel(dst, dst, -1, 1, 1, 7);
   cv::inRange(dst, cv::Scalar(0, 0, 255), cv::Scalar(0, 0, 255), dst);
   cv::dilate(dst, dst, dilateKernel);
   cv::erode(dst, dst, erodeKernel, cv::Point(-1, -1), 2);
   cv::Canny(dst, dst, 0, 255, 3);
+  transformUsingHough(dst);
   return dst;
+}
+
+void transformUsingHough(cv::Mat img)
+{
+  std::vector<cv::Vec4i> lines;
+  cv::Mat color_dst;
+  cv::cvtColor( img, color_dst, CV_GRAY2BGR );
+  cv::HoughLinesP(img, lines, 1, CV_PI/180, thresholdValue, minLineValue, maxLineValue);
+  for(int i = 0; i < lines.size(); i++ )
+    {
+       cv::Scalar clr(rand() % 255,rand() % 255,rand() % 255);
+        cv::line( color_dst, cv::Point(lines[i][0], lines[i][1]), cv::Point(lines[i][2], lines[i][3]), clr, 1, 8 );
+    }
+    cv::imshow("Lines", color_dst);
 }
 
 std::vector<std::vector<cv::Point> > findBiggestThree(cv::Mat cannyMatrix)
