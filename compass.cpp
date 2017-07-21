05c963df0bbe6ce3b1c4c850f8e052d7aa6894ef
Pooja <19swamipo@fpsct.org>
2017-07-21 15:04:34 -0400

    read and write data for compass

--- compass.cpp
+++ compass.cpp
@@ -5,14 +5,31 @@
 #include <linux/i2c-dev.h>
 #include <ncurses.h>
 
-#define I2C_ADDR 1234
+#define I2C_ADDR 0x1E
 
 int handle;
+unsigned char command[2];
 
 void compassInit();
-void readCompass(unsigned char[4]&);
+void readCompass(int&, int&, int&);
 
 void compassInit()
 {
   handle = i2cOpen(1, I2C_ADDR, 0);
+  command[0] = 0x00;
+  command[1] = 0xB8;
+  i2cWriteDevice(handle, command&, 2);
+}
+
+void readCompass(int x&, int y&, int z&)
+{
+  unsigned char value[6];
+  const int firstByte = 0x03;
+  for(i = 0; i < 6; i++)
+  {
+    value[i] = i2cReadByteData(handle, firstByte+i);
+  }
+  x = (value[0] << 8) | value[1];
+  z = (value[2] << 8) | value[3];
+  y = (value[4] << 8) | value[5];
 }
