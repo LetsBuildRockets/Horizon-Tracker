49a65b33d1bb3da83b812b7a4831a3a396159c21
Eric Sims <ericsimsm@gmail.com>
2017-08-09 14:05:34 -0400

    fixed up make file

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
-horizonTracker: horizonTracker.cpp
+horizonTracker: horizonTracker.cpp i2c.o
 	g++ horizonTracker.cpp -o horizonTracker -std=c++11 -lpthread -Ofast `pkg-config --cflags --libs opencv`
 
+i2c.o: i2c.cpp
+	g++ i2c.cpp -o i2c.o
+
 clean:
-	rm horizonTracker
+	rm horizonTracker i2c.o
--- i2c.cpp
+++ i2c.cpp
@@ -5,7 +5,7 @@
 #include <iostream>
 #include <linux/i2c-dev.h>
 
-#define I2C_ADDR 0x1E
+#define I2C_ADDR_COMPASS 0x1E
 
 int file_i2c;
 int length;
@@ -20,9 +20,9 @@ int main()
 {
     int x, y, z;
     openi2c();
+    compassInit();
     while(1)
     {
-      compassInit();
       readCompass(x, y, z);
       std::cout << "x: " << x/2048.0*360 << " y: " << y/2048.0*360 << " z: " << z/2048.0*360 << std::endl ;
       usleep(1000000);
@@ -32,25 +32,21 @@ void openi2c() {
   char *filename = (char*)"/dev/i2c-1";
 	if ((file_i2c = open(filename, O_RDWR)) < 0)
 	{
-		//ERROR HANDLING: you can check errno to see what went wrong
 		printf("Failed to open the i2c bus");
 		return;
 	}
 }
 void compassInit()
 {
-  if (ioctl(file_i2c, I2C_SLAVE, I2C_ADDR) < 0)
+  if (ioctl(file_i2c, I2C_SLAVE, I2C_ADDR_COMPASS) < 0)
   {
     printf("Failed to acquire bus access and/or talk to slave.\n");
-    //ERROR HANDLING; you can check errno to see what went wrong
     return;
   }
   buffer[0] = 0x00;
   buffer[1] = 0x90;
-  length = 2;			//<<< Number of bytes to write
-  if (write(file_i2c, buffer, length) != length)		//write() returns the number of bytes actually written, if it doesn't match then an error occurred (e.g. no response from the device)
-  {
-  		/* ERROR HANDLING: i2c transaction failed */
+  length = 2;
+  if (write(file_i2c, buffer, length) != length) {
   		printf("1) Failed to write to the i2c bus.\n");
   }
   usleep(100000);
